037b580eaa6c5b08d09d0eb40cfd5c9ff4e7f8d1
dadlessnsad <tbodnar85@gmail.com>
2023-11-23 08:09:09 -0700

    Membership Tier Check

--- routes/dagoraMemberships/Memberships.go
+++ routes/dagoraMemberships/Memberships.go
@@ -56,3 +56,180 @@ func CheckAddressHasMembership(clts *models.Clients) http.HandlerFunc {
 		return
 	}
 }
+
+func CheckAddressHoldEcclesia(clts *models.Clients) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet && r.Method != http.MethodPost {
+			helpers.RespondWithError(w, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+
+		address := r.URL.Query().Get("address")
+		isValid := helpers.IsValidAddress(address)
+		if isValid != true {
+			helpers.RespondWithError(w, http.StatusBadRequest, "Invalid address")
+			return
+		}
+		commonAddress := common.HexToAddress(address)
+		contractAddress := contracts.NFTContracts["Dagora Membership"].Address
+		optimsimClient := clts.OptimismClient.Client
+
+		membershipInstance, err := dagoramemberships.NewDagoramembershipsabi(contractAddress, optimsimClient)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		tokenId, err := membershipInstance.AddressTokenIds(nil, commonAddress)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		if tokenId == big.NewInt(0) {
+			helpers.RespondWithJSON(w, http.StatusOK, false)
+			return
+		}
+
+		// check that tokenId is ecclesia tier
+		tier, err := membershipInstance.GetMembershipTier(nil, tokenId)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		// tiers are
+		// 0 - ecclesia
+		// 1 - dagora
+		// 2 - hoplite
+		// 3 - perclesian
+		// if tier is 0, then return true
+		if tier == 0 {
+			helpers.RespondWithJSON(w, http.StatusOK, true)
+			return
+		}
+		helpers.RespondWithJSON(w, http.StatusOK, false)
+		return
+	}
+}
+
+func CheckAddressHoldsHoplite(clts *models.Clients) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet && r.Method != http.MethodPost {
+			helpers.RespondWithError(w, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+
+		address := r.URL.Query().Get("address")
+		isValid := helpers.IsValidAddress(address)
+		if isValid != true {
+			helpers.RespondWithError(w, http.StatusBadRequest, "Invalid address")
+			return
+		}
+		commonAddress := common.HexToAddress(address)
+		contractAddress := contracts.NFTContracts["Dagora Membership"].Address
+		optimsimClient := clts.OptimismClient.Client
+
+		membershipInstance, err := dagoramemberships.NewDagoramembershipsabi(contractAddress, optimsimClient)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		tokenId, err := membershipInstance.AddressTokenIds(nil, commonAddress)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		if tokenId.Cmp(big.NewInt(0)) == 0 {
+			helpers.RespondWithJSON(w, http.StatusOK, false)
+			return
+		}
+
+		// check that tokenId is hoplite tier
+		tier, err := membershipInstance.GetMembershipTier(nil, tokenId)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		// tiers are
+		// 0 - ecclesia
+		// 1 - dagora
+		// 2 - hoplite
+		// 3 - perclesian
+		// if tier is 2, then return true
+		if tier == 2 {
+			helpers.RespondWithJSON(w, http.StatusOK, true)
+			return
+		}
+		helpers.RespondWithJSON(w, http.StatusOK, false)
+		return
+	}
+}
+
+func CheckAddressHoldsPerclesia(clts *models.Clients) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet && r.Method != http.MethodPost {
+			helpers.RespondWithError(w, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+
+		address := r.URL.Query().Get("address")
+		isValid := helpers.IsValidAddress(address)
+		if isValid != true {
+			helpers.RespondWithError(w, http.StatusBadRequest, "Invalid address")
+			return
+		}
+		commonAddress := common.HexToAddress(address)
+		contractAddress := contracts.NFTContracts["Dagora Membership"].Address
+		optimsimClient := clts.OptimismClient.Client
+
+		membershipInstance, err := dagoramemberships.NewDagoramembershipsabi(contractAddress, optimsimClient)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		tokenId, err := membershipInstance.AddressTokenIds(nil, commonAddress)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		if tokenId.Cmp(big.NewInt(0)) == 0 {
+			helpers.RespondWithJSON(w, http.StatusOK, false)
+			return
+		}
+
+		// check that tokenId is hoplite tier
+		tier, err := membershipInstance.GetMembershipTier(nil, tokenId)
+		if err != nil {
+			log.Println(err)
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Internal server error")
+			return
+		}
+
+		// tiers are
+		// 0 - ecclesia
+		// 1 - dagora
+		// 2 - hoplite
+		// 3 - perclesian
+		// if tier is 3, then return true
+		if tier == 3 {
+			helpers.RespondWithJSON(w, http.StatusOK, true)
+			return
+		}
+		helpers.RespondWithJSON(w, http.StatusOK, false)
+		return
+	}
+}
